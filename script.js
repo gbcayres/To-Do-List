a1bc484e73bff491c5463735a0fd891adc4f41da
gb <gabriel.cayres.r@gmail.com>
2024-04-09 14:44:39 -0300

    refined features

--- script.js
+++ script.js
@@ -1,25 +1,57 @@
 const $input = document.querySelector('.header input');
 const $addBtn = document.querySelector('.header button');
 const $todoList = document.querySelector('.todo-list');
-const $todos = document.querySelectorAll('.todo-list li');
-const $clearAllBtn = document.querySelector('.clearAll');
-const $clearCompleted = document.querySelector('.clearCompleted');
+const $clearAllBtn = document.querySelector('.clear-all');
+const $clearCompleted = document.querySelector('.clear-completed');
+
+document.addEventListener("DOMContentLoaded",  () => {
+    loadTodos();
+})
 
 $addBtn.addEventListener('click', addTask)
 
-$todos.forEach((todo) => {
-    todo.addEventListener('click', checkTodo)
-})
-
-$clearAllBtn.addEventListener('click', removeTodos)
+$clearAllBtn.addEventListener('click', removeAll)
 
 $clearCompleted.addEventListener('click', removeCompleted)
 
-function addTask() {
-    const newTask =  document.createElement('li');
-    newTask.innerHTML = $input.value;
+$input.addEventListener('keypress', (e) => {
+    if(e.key === 'Enter') {
+        addTask();
+    }
+})
 
-    $todoList.append(newTask);
+function addTask() {
+    const taskText = $input.value.trim();
+
+    if(taskText) {
+        const newTask =  createTaskElement(taskText)
+        $todoList.appendChild(newTask)
+        saveTodos();
+        $input.value = '';
+        $input.focus();
+    }
+}
+
+function toggleTask(e) {
+    const task = e.target;
+    task.classList.toggle('checked')
+}
+
+function createTaskElement(taskText) {
+    const newTask = document.createElement('li');
+    newTask.textContent = taskText;
+    newTask.addEventListener('click', toggleTask);
+    const closeIcon = createCloseIcon();
+    newTask.appendChild(closeIcon);
+    return newTask;
+}
+
+function createCloseIcon() {
+    const closeIcon = document.createElement('img');
+    closeIcon.src = "./assets/images/close.svg";
+    closeIcon.classList.add('close-icon');
+    closeIcon.addEventListener('click', removeTask);
+    return closeIcon;
 }
 
 function checkTodo(e) {
@@ -27,14 +59,31 @@ function checkTodo(e) {
     todo.classList.toggle('checked');
 }
 
-function removeTodos() {
+function removeTask(e) {
+    const todo = e.target;
+    todo.parentElement.remove()
+    saveTodos();
+}
+
+function removeAll() {
     $todoList.innerHTML = '';
+    saveTodos();
 }
 
 function removeCompleted() {
-    $todos.forEach(todo => {
+    const todos = document.querySelectorAll('.todo-list li');
+    todos.forEach(todo => {
         if (todo.classList.contains('checked')) {
             todo.remove();
         }
     });
+}
+
+function saveTodos() {
+    const todoList = $todoList;
+    localStorage.setItem('todos', todoList.innerHTML)
+}
+
+function loadTodos() {
+    $todoList.innerHTML = localStorage.getItem('todos') || '';
 }
